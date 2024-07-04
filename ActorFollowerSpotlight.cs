9d5c24caea961d71da37f0de14e4d4430daedb73
Antonia <62192270+Phantonia@users.noreply.github.com>
2024-07-04 18:49:50 +0200

    Something big begins

---
+++ ActorFollowerSpotlight.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class ActorFollowerSpotlight : SpotLight3D
+{
+	const float HalfLife = 0.1F;
+
+	[Export] public MeshInstance3D Actor { get; set; }
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+        Vector3 pivot = Vector3.Forward;
+
+		Vector3 target = Actor.Position + new Vector3(0, ((CapsuleMesh)Actor.Mesh).Height, 0);
+
+		Vector3 direction = (target - Position).Normalized();
+        Vector3 axis = pivot.Cross(direction).Normalized();
+		float angle = Mathf.Acos(direction.Dot(pivot));
+
+		Quaternion goal = new Quaternion(axis, angle);
+		Quaternion = Quaternion.Slerp(goal, Mathf.Clamp(8F * (float)delta, 0, 1));
+	}
+}
